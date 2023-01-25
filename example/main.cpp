298cf71455fbb48c99c4e0db306109519e4090d1
ptrvsrg <s.petrov1@g.nsu.ru>
2023-01-25 22:26:21 +0700

    Add example

--- example/main.cpp
+++ example/main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include "cl_parser.h"
+#include "game.h"
 
-int main()
+int main(int argc, char ** argv)
 {
+    try
+    {
+        Options opts;
+        GetOptions(argc, argv, opts);
+
+        Game game(opts.m_input_file,
+                  opts.m_output_file,
+                  opts.m_iterations);
+        game.Launch();
+    }
+    catch (const std::exception & ex)
+    {
+        std::cerr << ex.what();
+    }
+
     return EXIT_SUCCESS;
 }

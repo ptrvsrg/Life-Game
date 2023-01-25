d78cb9c6d83319010243be9501aacb8f6f20d313
ptrvsrg <s.petrov1@g.nsu.ru>
2023-01-25 22:22:49 +0700

    Update mode creator

--- lib/mode/mode_creator/src/mode_creator.cpp
+++ lib/mode/mode_creator/src/mode_creator.cpp
@@ -4,10 +4,16 @@
 
 ModePtr AbstractMode::CreateMode(const std::string & input_file,
                                  const std::string & output_file,
-                                 unsigned int iterations)
+                                 int iterations)
 {
     if (!input_file.empty() && !output_file.empty() && iterations != -1)
-        return ;
+        return std::make_unique<OfflineMode>(input_file, output_file, iterations);
     else
-        return ;
+    {
+        // Warnings
+        if (!output_file.empty() || iterations != -1)
+            std::clog << "Warnings: There are not enough parameters to start offline mode\n";
+
+        return std::make_unique<OnlineMode>(input_file);
+    }
 }
